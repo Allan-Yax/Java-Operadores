db06d7481dae1d473205fc9c26ca2b4bc604c0fe
Allan Yax <allanyax0@gmail.com>
2024-12-30 11:32:09 -0600

    Se agrego proyecto a git

---
+++ src/Operadores.java
@@ -0,0 +1,51 @@
+public class Operadores {
+    public static void main(String[] args) {
+        // Operadores aritmeticos (1)
+//        int a = 3, b = 2, resultado = 0;
+//        // Suma
+//        resultado = a + b;
+//        System.out.println("Suma = " + resultado);
+//        // Resta
+//        resultado = a - b;
+//        System.out.println("Resta = " + resultado);
+//        // Multiplicacion
+//        resultado = a * b;
+//        System.out.println("Mulriplicacion = " + resultado);
+//        // Division
+//        resultado = a / b;
+//        System.out.println("Division = " + resultado);
+//        // Modulo (residuo division)
+//        resultado = a % b;
+//        System.out.println("resultado = " + resultado);
+
+        //Operadores Unarios (2)
+//        int a = 3, b = -2, resultado =0;
+//        var c = true;
+//        // Operador unitario -
+//        resultado = -a;
+//        System.out.println("-a = " + resultado);
+//        resultado = -b;
+//        System.out.println("-b = " + resultado);
+//        //operador unario ++ (incremento en uno)
+//        a = 3;
+//        resultado = ++a; //pre-incremento
+//        System.out.println("Pre-incremento ++a = " + resultado);
+//        a = 3;
+//        resultado = a++; //post-incremento
+//        System.out.println("Post-incremento a++ = " + resultado);
+//        System.out.println("a = " + a);
+//        // Operador unario -- (decrementa en uno)
+//        b = -2;
+//        resultado = --b; //pre-decremento
+//        System.out.println("Pre-decremento --b = " + resultado);
+//        b = -2;
+//        resultado = b--; //post-decremento
+//        System.out.println("Post-decremento b-- = " + resultado);
+//        System.out.println("b " + b);
+
+        // Negacion ! (invierte el valor logico)
+        var c = true;
+        var resultado2 = !c;
+        System.out.println("Negacion !c = " + resultado2);
+    }
+}
